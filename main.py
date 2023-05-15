6cb0a27ca75a07302f61e32c08f3abf820fe4dac
unknown <smartdevpro001@gmail.com>
2023-05-16 00:34:54 +0900

    3 chain tracker

--- main.py
+++ main.py
@@ -2,27 +2,44 @@ import time
 import os.path
 import re
 from util import *
+import requests
 
 last_block_arb_number = 0
-
+last_block_eth_number = 0
+last_block_bnb_number = 0
 
 # Define some helper functions
-def get_wallet_transactions(wallet_address):
-    url = f'https://api.arbiscan.io/api?module=account&action=txlist&address={wallet_address}&startblock={last_block_arb_number}&endblock=99999999&sort=desc&apikey={ARBICAN_API_KEY}'
+def get_wallet_transactions(wallet_address,blockchain):
+    if blockchain == 'eth':
+        url = f'https://api.etherscan.io/api?module=account&action=txlist&address={wallet_address}&startblock={last_block_eth_number}&endblock=99999999&sort=desc&apikey={ETH_API_KEY}'
+    elif blockchain == 'bnb':
+        url = f'https://api.bscscan.com/api?module=account&action=txlist&address={wallet_address}&startblock={last_block_bnb_number}&endblock=99999999&sort=desc&apikey={BSC_API_KEY}'
+    elif blockchain == 'bnb':
+        url = f'https://api.arbiscan.io/api?module=account&action=txlist&address={wallet_address}&startblock={last_block_arb_number}&endblock=99999999&sort=desc&apikey={ARBICAN_API_KEY}'
+    else:
+        raise ValueError('Invalid blockchain specified')
+
     response = requests.get(url)
     data = json.loads(response.text)
 
     result = data.get('result', [])
     if not isinstance(result, list):
         print(
-            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Error fetching transactions for {wallet_address} on {BLOCKCHAIN_NAME} blockchain: {data}")
+            f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Error fetching transactions for {wallet_address} on {blockchain} blockchain: {data}")
         return []
 
     return result
 
+def send_telegram_notification(message, value, usd_value, tx_hash, blockchain):
+    if blockchain == 'eth':
+        etherscan_link = f'<a href="https://etherscan.io/tx/{tx_hash}">Etherscan</a>'
+    elif blockchain == 'bnb':
+        etherscan_link = f'<a href="https://bscscan.com/tx/{tx_hash}">BscScan</a>'
+    elif blockchain == 'arb':
+        etherscan_link = f'<a href="https://arbiscan.io/tx/{tx_hash}">ARBISCAN</a>'
+    else:
+        raise ValueError('Invalid blockchain specified')
 
-def send_telegram_notification(message, value, usd_value, tx_hash):
-    etherscan_link = f'<a href="https://arbiscan.io/tx/{tx_hash}">ARBISCAN</a>'
 
     url = f'https://api.telegram.org/bot{TELEGRAM_BOT_TOKEN}/sendMessage'
     payload = {'chat_id': f'{TELEGRAM_CHAT_ID}',
@@ -31,12 +48,26 @@ def send_telegram_notification(message, value, usd_value, tx_hash):
 
     response = requests.post(url, data=payload)
     print(
-        f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Telegram notification sent with message: {message}, value: {value} {BLOCKCHAIN_NAME} (${usd_value:.2f})")
+        f"[{time.strftime('%Y-%m-%d %H:%M:%S')}] Telegram notification sent with message: {message}, value: {value} {blockchain} (${usd_value:.2f})")
     return response
 
 
 def get_current_block_number():
     # Get ETH block number
+    url = f'https://api.etherscan.io/api?module=proxy&action=eth_blockNumber&apikey={ETH_API_KEY}'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    result = data.get('result', [])
+    global last_block_eth_number
+    global last_block_bnb_number
+    last_block_eth_number = int(result, 0)
+    # Get BNB block number
+    url = f'https://api.bscscan.com/api?module=proxy&action=eth_blockNumber&apikey={BSC_API_KEY}'
+    response = requests.get(url)
+    data = json.loads(response.text)
+    result = data.get('result', [])
+    last_block_bnb_number = int(result, 0)
+    # Get ARB block number
     url = f'https://api.arbiscan.io/api?module=proxy&action=eth_blockNumber&apikey={ARBICAN_API_KEY}'
     response = requests.get(url)
     data = json.loads(response.text)
@@ -47,22 +78,23 @@ def get_current_block_number():
 
 def monitor_wallets():
     get_current_block_number()
-    file_path = "log/watched_wallets.txt"
+    file_path = "watched_wallets.txt"
     if not os.path.exists(file_path):
         open(file_path, 'w').close()
 
     latest_tx_hashes = {}
-    latest_tx_hashes_path = "log/latest_tx_hashes.json"
+    latest_tx_hashes_path = "latest_tx_hashes.json"
     if os.path.exists(latest_tx_hashes_path):
         with open(latest_tx_hashes_path, "r") as f:
             latest_tx_hashes = json.load(f)
 
     last_run_time = 0
-    last_run_time_path = "log/last_run_time.txt"
+    last_run_time_path = "last_run_time.txt"
     if os.path.exists(last_run_time_path):
         with open(last_run_time_path, "r") as f:
             last_run_time = int(f.read())
     eth_usd_price = get_eth_price()
+    bnb_usd_price = get_bnb_price()
     while True:
         try:
             # Read from file
@@ -71,7 +103,7 @@ def monitor_wallets():
 
             for wallet in watched_wallets:
                 blockchain, wallet_address = wallet.split(':')
-                transactions = get_wallet_transactions(wallet_address)
+                transactions = get_wallet_transactions(wallet_address,blockchain)
                 for tx in transactions:
                     tx_hash = tx['hash']
                     tx_time = int(tx['timeStamp'])
@@ -79,7 +111,17 @@ def monitor_wallets():
                     if tx_hash not in latest_tx_hashes and tx_time > last_run_time:
 
                         global last_block_arb_number
-                        last_block_arb_number = int(tx['blockNumber'])
+                        global last_block_eth_number
+                        global last_block_bnb_number
+                        if blockchain == "eth":
+                            last_block_eth_number = int(tx['blockNumber'])
+                        elif blockchain == "bnb":
+                            last_block_bnb_number = int(tx['blockNumber'])
+                        elif blockchain == "arb":
+                            last_block_arb_number = int(tx['blockNumber'])
+                        else:
+                            continue
+
 
                         contract_address = tx['to']
                         method_id = tx['methodId']
@@ -87,7 +129,35 @@ def monitor_wallets():
                         behavious = 'BUY'
                         value = float(tx['value']) / 10 ** 18  # Convert from wei to ETH or BNB
 
-                        if contract_address.lower() == ARITRAM_ROUTER_ADDRESS.lower() and method_id == '0x04e45aaf':
+                        if contract_address.lower() == ETH_ROUTER_ADDRESS.lower() or contract_address.lower() == BSC_ROUTER_ADDRESS.lower():
+                            try:
+                                eth_usd_price = get_eth_price()
+                                bnb_usd_price = get_bnb_price()
+                            except:
+                                print("Because of api server, we are using old price")
+                            usd_value = value * eth_usd_price if blockchain == "eth" else value * bnb_usd_price
+
+                            print('tx:{}\nchain:{}\n'.format(tx_hash, blockchain))
+                            # buy function
+                            if method_id == '0xfb3bdb41' or method_id == '0x7ff36ab5' or method_id == '0xb6f9de95':
+                                token_address = '0x' + input[-40:]
+                                token_name = get_tokensymbol(token_address,blockchain)
+                            # sell function
+                            elif method_id == '0x18cbafe5' or method_id == '0x4a25d94a' or method_id == '0x791ac947':
+                                token_address = '0x' + input[-104:-64]
+                                token_name = get_tokensymbol(token_address, blockchain)
+                                behavious = "SELL"
+                            # token exchange
+                            elif method_id == '0x38ed1739' or method_id == '0x8803dbee' or method_id == '0x5c11d795':
+                                token1_address = '0x' + input[-104:-64]
+                                token_name = get_tokensymbol(token1_address, blockchain)
+                                behavious = "TOKEN SWAP"
+                            else:
+                                continue
+                            message = f'🚨 Wallet:{wallet_address}\nBehaviour:{behavious}\nToken name:{token_name}\nChain:{blockchain}\nToken Address:{token_address}\n'
+                            send_telegram_notification(message, value, usd_value, tx['hash'], blockchain)
+
+                        if contract_address.lower() == ARITRAM_ROUTER_ADDRESS.lower() and method_id == '0x04e45aaf' and blockchain == "arb":
                             print("TX:{}".format(tx_hash))
                             try:
                                 eth_usd_price = get_eth_price()
@@ -99,8 +169,8 @@ def monitor_wallets():
                             # buy function
                             token1_addess = input[34:74]
                             token2_addess = input[98:138]
-                            token1_name = get_eth_tokensymbol(token1_addess)
-                            token2_name = get_eth_tokensymbol(token2_addess)
+                            token1_name = get_tokensymbol(token1_addess,blockchain)
+                            token2_name = get_tokensymbol(token2_addess,blockchain)
 
                             # buy function
                             if token1_name == "WETH":
@@ -113,8 +183,8 @@ def monitor_wallets():
                                 token_address = token2_addess
                             else:
                                 continue
-                            message = f'🚨 Wallet:{wallet_address}\nBehaviour:{behavious}\nToken name:{token_name}\nChain:{BLOCKCHAIN_NAME}\nToken Address:{token_address}\n'
-                            send_telegram_notification(message, value, usd_value, tx['hash'], BLOCKCHAIN_NAME)
+                            message = f'🚨 Wallet:{wallet_address}\nBehaviour:{behavious}\nToken name:{token_name}\nChain:{blockchain}\nToken Address:{token_address}\n'
+                            send_telegram_notification(message, value, usd_value, tx['hash'], blockchain)
                         latest_tx_hashes[tx_hash] = int(tx['blockNumber'])
 
             # Save latest_tx_hashes to file
@@ -135,46 +205,41 @@ def monitor_wallets():
 
 
 def add_wallet(wallet_address, blockchain):
-    if blockchain == 'arb':
-        file_path = "log/watched_wallets.txt"
-        with open(file_path, 'a') as f:
-            f.write(f'{blockchain}:{wallet_address}\n')
+    file_path = "log/watched_wallets.txt"
+    with open(file_path, 'a') as f:
+        f.write(f'{blockchain}:{wallet_address}\n')
 
 
 def remove_wallet(wallet_address, blockchain):
-    if blockchain == 'arb':
-        file_path = "log/watched_wallets.txt"
-        temp_file_path = "temp.txt"
-        with open(file_path, 'r') as f, open(temp_file_path, 'w') as temp_f:
-            for line in f:
-                if line.strip() != f'{blockchain}:{wallet_address}':
-                    temp_f.write(line)
-        os.replace(temp_file_path, file_path)
+    file_path = "log/watched_wallets.txt"
+    temp_file_path = "temp.txt"
+    with open(file_path, 'r') as f, open(temp_file_path, 'w') as temp_f:
+        for line in f:
+            if line.strip() != f'{blockchain}:{wallet_address}':
+                temp_f.write(line)
+    os.replace(temp_file_path, file_path)
 
 
 # Define the command handlers for the Telegram bot
 def start(update, context):
-    pass
-
-
-#     message = """
-# 👋 Welcome to the Ethereum and Binance Wallet Monitoring Bot!
-#
-# Use /add <blockchain> <wallet_address> to add a new wallet to monitor.
-#
-# Example: /add ETH 0x123456789abcdef
-#
-# Use /remove <blockchain> <wallet_address> to stop monitoring a wallet.
-#
-# Example: /remove ETH 0x123456789abcdef
-#
-# Use /list <blockchain> to list all wallets being monitored for a specific blockchain.
-#
-# Example: /list ETH or just /list
-#
-# Don't forget to star my Github repo if you find this bot useful! https://github.com/cankatx/crypto-wallet-tracker ⭐️
-#     """
-#     context.bot.send_message(chat_id=update.message.chat_id, text=message)
+    message = """
+    👋 Welcome to the Ethereum and Binance Wallet Monitoring Bot!
+    
+    Use /add <blockchain> <wallet_address> to add a new wallet to monitor.
+    
+    Example: /add ETH 0x123456789abcdef
+    
+    Use /remove <blockchain> <wallet_address> to stop monitoring a wallet.
+    
+    Example: /remove ETH 0x123456789abcdef
+    
+    Use /list <blockchain> to list all wallets being monitored for a specific blockchain.
+    
+    Example: /list ETH or just /list
+    
+    Don't forget to star my Github repo if you find this bot useful! https://github.com/cankatx/crypto-wallet-tracker ⭐️
+        """
+    context.bot.send_message(chat_id=update.message.chat_id, text=message)
 
 
 def add(update, context):
@@ -187,10 +252,20 @@ def add(update, context):
     wallet_address = context.args[1]
 
     # Check if the wallet address is in the correct format for the specified blockchain
-    if blockchain == 'arb':
+    if blockchain == 'eth':
         if not re.match(r'^0x[a-fA-F0-9]{40}$', wallet_address):
             context.bot.send_message(chat_id=update.message.chat_id,
                                      text=f"{wallet_address} is not a valid Ethereum wallet address.")
+            return
+    elif blockchain == 'bnb':
+        if not re.match(r'^0x[a-fA-F0-9]{40}$', wallet_address):
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                     text=f"{wallet_address} is not a valid Binance Smart Chain wallet address.")
+            return
+    elif blockchain == 'arb':
+        if not re.match(r'^0x[a-fA-F0-9]{40}$', wallet_address):
+            context.bot.send_message(chat_id=update.message.chat_id,
+                                     text=f"{wallet_address} is not a valid Arbitram wallet address.")
             return
     else:
         context.bot.send_message(chat_id=update.message.chat_id, text=f"Invalid blockchain specified: {blockchain}")
@@ -218,16 +293,31 @@ def list_wallets(update, context):
         wallets = [line.strip() for line in f.readlines()]
     if wallets:
         eth_wallets = []
+        bsc_wallets = []
+        arb_wallets = []
         for wallet in wallets:
             blockchain, wallet_address = wallet.split(':')
-            if blockchain == 'arb':
+            if blockchain == 'eth':
                 eth_wallets.append(wallet_address)
+            elif blockchain == 'bnb':
+                bsc_wallets.append(wallet_address)
+            elif blockchain == 'arb':
+                arb_wallets.append(wallet_address)
 
         message = "The following wallets are currently being monitored\n"
         message += "\n"
         if eth_wallets:
-            message += "ARBITRAM Wallets:\n"
+            message += "Ethereum Wallets:\n"
             for i, wallet in enumerate(eth_wallets):
+                message += f"{i + 1}. {wallet}\n"
+            message += "\n"
+        if bsc_wallets:
+            message += "Binance Coin Wallets:\n"
+            for i, wallet in enumerate(bsc_wallets):
+                message += f"{i + 1}. {wallet}\n"
+        if arb_wallets:
+            message += "ARBITRAM Wallets:\n"
+            for i, wallet in enumerate(arb_wallets):
                 message += f"{i + 1}. {wallet}\n"
             message += "\n"
         context.bot.send_message(chat_id=update.message.chat_id, text=message)
--- util.py
+++ util.py
@@ -1,31 +1,45 @@
 from web3 import Web3
 import json
-import requests
 
 # Update the following variables with your own Etherscan and BscScan API keys and Telegram bot token
 TELEGRAM_BOT_TOKEN = '5800403587:AAHHpzfKWVyheYYIWcMHpUJsMlqIhQ7xoTM'
 TELEGRAM_CHAT_ID = '-984543212'
 BLOCKCHAIN_NAME = "ARBITRAM"
 
-# Router address
+# Arbitram network
 ARBICAN_API_KEY = 'CIQC9D6IWVXQ1ZX8MJ53SJTE7JJZM875HE'
 ARITRAM_ROUTER_ADDRESS = '0x68b3465833fb72a70ecdf485e0e4c7bd8665fc45'
 ARITRAM_USDT_ADDRESS = '0xFd086bC7CD5C481DCC9C85ebE478A1C0b69FCbb9'
 ARITRAM_WETH_ADDRESS = '0x82af49447d8a07e3bd95bd0d56f35241523fbab1'
 ARITRAM_RPC = 'https://arb1.arbitrum.io/rpc'
+arb_eth_w3 = Web3(Web3.HTTPProvider(ARITRAM_RPC))
+arb_eth_router = Web3.to_checksum_address(ARITRAM_ROUTER_ADDRESS)
+arb_eth_usdt = Web3.to_checksum_address(ARITRAM_USDT_ADDRESS)
+arb_eth_weth = Web3.to_checksum_address(ARITRAM_WETH_ADDRESS)
+
+
+# ETHEREUM
+ETH_API_KEY = 'IPMS9STM3WNJ4K55KR6FS1F1KB77VPI96K'
+ETH_ROUTER_ADDRESS = '0x7a250d5630b4cf539739df2c5dacb4c659f2488d'
+ETH_USDT_ADDRESS = '0xdac17f958d2ee523a2206206994597c13d831ec7'
+ETH_WETH_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
 ETH_RPC = 'https://mainnet.infura.io/v3/9aa3d95b3bc440fa88ea12eaa4456161'
+eth_w3 = Web3(Web3.HTTPProvider(ETH_RPC))
+eth_router = Web3.to_checksum_address(ETH_ROUTER_ADDRESS)
+eth_usdt = Web3.to_checksum_address(ETH_USDT_ADDRESS)
+eth_weth = Web3.to_checksum_address(ETH_WETH_ADDRESS)
 
-original_eth_usdt = Web3.to_checksum_address('0xdac17f958d2ee523a2206206994597c13d831ec7')
-original_eth_weth = Web3.to_checksum_address('0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2')
-original_eth_router = Web3.to_checksum_address('0x7a250d5630b4cf539739df2c5dacb4c659f2488d')
+# BINANCE
+BSC_API_KEY = '97H84KY4GJ81VK591B3TKSFXG4ANTWYRV3'
+BSC_ROUTER_ADDRESS = '0x10ED43C718714eb63d5aA57B78B54704E256024E'
+BSC_USDT_ADDRESS = '0x55d398326f99059ff775485246999027b3197955'
+BSC_WETH_ADDRESS = '0xbb4cdb9cbd36b01bd1cbaebf2de08d9173bc095c'
+BSC_RPC = 'https://bsc-dataseed1.binance.org:443'
+bsc_w3 = Web3(Web3.HTTPProvider(BSC_RPC))
+bsc_router = Web3.to_checksum_address(BSC_ROUTER_ADDRESS)
+bsc_usdt = Web3.to_checksum_address(BSC_USDT_ADDRESS)
+bsc_weth = Web3.to_checksum_address(BSC_WETH_ADDRESS)
 
-# Connect to the Ethereum network
-eth_w3 = Web3(Web3.HTTPProvider(ARITRAM_RPC))
-eth_router = Web3.to_checksum_address(ARITRAM_ROUTER_ADDRESS)
-eth_usdt = Web3.to_checksum_address(ARITRAM_USDT_ADDRESS)
-eth_weth = Web3.to_checksum_address(ARITRAM_WETH_ADDRESS)
-
-orginal_eth_w3 = Web3(Web3.HTTPProvider(ETH_RPC))
 
 Round = lambda x, n: float(eval('"%.'+str(int(n))+'f" % '+repr(int(x)+round(float('.'+str(float(x)).split('.')[1]),n))))
 
@@ -37,19 +51,31 @@ router_abi = ''
 with open('abi/router.json', 'r') as f:
     router_abi = json.load(f)
 
-
-def get_eth_tokensymbol(token_address):
+def get_tokensymbol(token_address,blockchain):
     # Create a contract instance for the token
     token_address = Web3.to_checksum_address(token_address)
-    token_contract = eth_w3.eth.contract(address=token_address, abi=eth_abi)
+    if blockchain == 'eth':
+        token_contract = eth_w3.eth.contract(address=token_address, abi=eth_abi)
+    elif blockchain == 'bsc':
+        token_contract = bsc_w3.eth.contract(address=token_address, abi=eth_abi)
+    elif blockchain == 'arb':
+        token_contract = arb_eth_w3.eth.contract(address=token_address, abi=eth_abi)
     # Call the name() function on the token contract
     token_name = token_contract.functions.symbol().call()
     return token_name
-print(get_eth_tokensymbol(ARITRAM_WETH_ADDRESS))
+
 def get_eth_price():
-    router_contract = orginal_eth_w3.eth.contract(address=original_eth_router, abi=router_abi)
+    router_contract = eth_w3.eth.contract(address=eth_router, abi=router_abi)
     oneToken = eth_w3.to_wei(1, 'Ether')
     price = router_contract.functions.getAmountsOut(oneToken,
-                                                    [original_eth_weth, original_eth_usdt]).call()
+                                                    [eth_weth, eth_usdt]).call()
     return Round((price[1]) / 10 ** 6,2)
+
+def get_bnb_price():
+    router_contract = bsc_w3.eth.contract(address=bsc_router, abi=router_abi)
+    oneToken = bsc_w3.to_wei(1, 'Ether')
+    price = router_contract.functions.getAmountsOut(oneToken,
+                                                    [bsc_weth, bsc_usdt]).call()
+    normalizedPrice = bsc_w3.from_wei(price[1], 'Ether') / oneToken
+    return Round((normalizedPrice) * 10 ** 18, 2)
 
